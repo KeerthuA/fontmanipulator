a5d74d4964ee59301c6f04690f19751de08d2c68
KeerthuA <77785377+KeerthuA@users.noreply.github.com>
2021-09-06 13:09:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(500,500);
+
+    canvas = createCanvas(500, 500);
+    canvas.position(510,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+    console.log('PoseNet is initialized');
+}
+function draw()
+{
+    background('#59bfff');
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
